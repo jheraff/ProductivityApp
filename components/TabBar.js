9495b6ac47103c6714a645105e6fd9c094e3a444
Jhermayne <sonothermes.com>
2025-02-03 18:49:15 -0800

    navbar fixed

--- components/TabBar.js
+++ components/TabBar.js
@@ -1,4 +1,3 @@
-
 import { StyleSheet, View, Text, TouchableOpacity, Image } from 'react-native';
 import React from 'react';
 
@@ -6,31 +5,31 @@ const TabBar = ({ state, descriptors, navigation }) => {
     const icons = {
         'home/index': (props) => (
             <Image
-                source={require('../assets/icons/HomeIcon.png')}
+                source={require('../assets/icons/home.png')}
                 style={{ width: props.width, height: props.height }}
             />
         ),
         'calendar/index': (props) => (
             <Image
-                source={require('../assets/icons/CalendarIcon.png')}
+                source={require('../assets/icons/calendar.png')}
                 style={{ width: props.width, height: props.height }}
             />
         ),
         'tasks/index': (props) => (
             <Image
-                source={require('../assets/icons/TaskIcon.png')}
+                source={require('../assets/icons/quest.png')}
                 style={{ width: props.width, height: props.height }}
             />
         ),
         'action/index': (props) => (
             <Image
-                source={require('../assets/icons/ActionIcon.png')}
+                source={require('../assets/icons/action.png')}
                 style={{ width: props.width, height: props.height }}
             />
         ),
         'profile/index': (props) => (
             <Image
-                source={require('../assets/icons/ProfileIcon.png')}
+                source={require('../assets/icons/settings.png')}
                 style={{ width: props.width, height: props.height }}
             />
         ),
@@ -39,80 +38,76 @@ const TabBar = ({ state, descriptors, navigation }) => {
     const primaryColor = 'black';
     const secondaryColor = 'white';
 
+    // List of allowed tabs
+    const allowedTabs = [
+        'home/index',
+        'calendar/index',
+        'tasks/index',
+        'action/index',
+        'profile/index'
+    ];
+
     return (
         <View style={styles.tabbar}>
-            {state.routes.map((route, index) => {
-                const { options } = descriptors[route.key];
-                const label =
-                    options.tabBarLabel !== undefined
-                        ? options.tabBarLabel
-                        : options.title !== undefined
-                            ? options.title
-                            : route.name;
+            {state.routes
+                .filter(route => allowedTabs.includes(route.name)) // Only show allowed routes
+                .map((route, index) => {
+                    const { options } = descriptors[route.key];
+                    const label =
+                        options.tabBarLabel !== undefined
+                            ? options.tabBarLabel
+                            : options.title !== undefined
+                                ? options.title
+                                : route.name;
 
-                // Check if the route is an auth route
-                if ([
-                    '_sitemap', 
-                    '+not-found', 
-                    '(auth)/login', 
-                    '(auth)/register', 
-                    '(pages)/home/index',
-                    '(pages)/tasks/index',
-                    '(pages)/calendar/index',
-                    '(pages)/action/index',
-                    '(pages)/settings/index'
-                
-                ].includes(route.name)) return null;
+                    const isFocused = state.index === index;
 
-                const isFocused = state.index === index;
+                    const onPress = () => {
+                        const event = navigation.emit({
+                            type: 'tabPress',
+                            target: route.key,
+                            canPreventDefault: true,
+                        });
 
-                const onPress = () => {
-                    const event = navigation.emit({
-                        type: 'tabPress',
-                        target: route.key,
-                        canPreventDefault: true,
-                    });
+                        if (!isFocused && !event.defaultPrevented) {
+                            navigation.navigate(route.name, route.params);
+                        }
+                    };
 
-                    if (!isFocused && !event.defaultPrevented) {
-                        navigation.navigate(route.name, route.params);
-                    }
-                };
+                    const onLongPress = () => {
+                        navigation.emit({
+                            type: 'tabLongPress',
+                            target: route.key,
+                        });
+                    };
 
-                const onLongPress = () => {
-                    navigation.emit({
-                        type: 'tabLongPress',
-                        target: route.key,
-                    });
-                };
+                    const Icon = icons[route.name];
 
-                const Icon = icons[route.name]; // Get the icon function
-
-                return (
-                    <TouchableOpacity
-                        key={route.name}
-                        style={styles.tabbarItem}
-                        accessibilityRole="button"
-                        accessibilityState={isFocused ? { selected: true } : {}}
-                        accessibilityLabel={options.tabBarAccessibilityLabel}
-                        testID={options.tabBarTestID}
-                        onPress={onPress}
-                        onLongPress={onLongPress}
-                    >
-                        {/* Check if Icon exists before calling it */}
-                        {Icon ? Icon({
-                            color: isFocused ? primaryColor : secondaryColor,
-                            width: 24,
-                            height: 24,
-                        }) : null}
-                        <Text style={{
-                            color: isFocused ? primaryColor : secondaryColor,
-                            fontSize: 11
-                        }}>
-                            {label}
-                        </Text>
-                    </TouchableOpacity>
-                );
-            })}
+                    return (
+                        <TouchableOpacity
+                            key={route.name}
+                            style={styles.tabbarItem}
+                            accessibilityRole="button"
+                            accessibilityState={isFocused ? { selected: true } : {}}
+                            accessibilityLabel={options.tabBarAccessibilityLabel}
+                            testID={options.tabBarTestID}
+                            onPress={onPress}
+                            onLongPress={onLongPress}
+                        >
+                            {Icon ? Icon({
+                                color: isFocused ? primaryColor : secondaryColor,
+                                width: 24,
+                                height: 24,
+                            }) : null}
+                            <Text style={{
+                                color: isFocused ? primaryColor : secondaryColor,
+                                fontSize: 11
+                            }}>
+                                {label}
+                            </Text>
+                        </TouchableOpacity>
+                    );
+                })}
         </View>
     );
 }
@@ -120,16 +115,17 @@ const TabBar = ({ state, descriptors, navigation }) => {
 const styles = StyleSheet.create({
     tabbar: {
         position: 'absolute',
-        bottom: 25,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        width: '100%', 
         flexDirection: 'row',
-        justifyContent: 'space-between',
+        justifyContent: 'space-around', 
         alignItems: 'center',
         backgroundColor: 'red',
-        marginHorizontal: 20,
         paddingVertical: 15,
-        borderRadius: 25,
         shadowColor: 'black',
-        shadowOffset: { width: 0, height: 10 },
+        shadowOffset: { width: 0, height: -3 },
         shadowRadius: 10,
         shadowOpacity: 0.1,
     },
